346994cd6d7f03f71b09b59fda73643e91802303
MWall <mjwall@student.fullsail.edu>
2021-09-15 14:32:09 -0400

    added header

---
+++ app/src/components/header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {BrowserRouter as Router, Link } from 'react-router-dom';
+
+import Home from '../../pages/home';
+import About from '../../pages/about';
+import Contact from '../../pages/contact';
+
+export default function Header() {
+    return (
+        <> 
+        <h1> Header </h1>
+        <nav>
+        <Router>
+            <Link to="/" >Home</Link>
+            <Link to="/about" >About</Link>
+            <Link to="/contact" >Contact</Link>
+        </Router>
+        </nav>
+        </>
+    )
+
+}
