9b81d3b1fd339e54db211d3a3ded38e42bbb3b2f
MWall <mjwall@student.fullsail.edu>
2021-09-15 14:32:12 -0400

    added header

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>App.js</h1>
+        <Header />
+        {/* <Footer /> */}
       </header>
     </div>
   );
