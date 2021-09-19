904a9c0bbbf4eb83c3d7d4ad76a5b892b0e55410
MWall <mjwall@student.fullsail.edu>
2021-09-19 14:21:16 -0400

    setting up navigation routes and links

--- app/src/components/header/index.js
+++ app/src/components/header/index.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import Home from '../../pages/home';
-import About from '../../pages/about';
-import Contact from '../../pages/contact';
+// import Home from '../../pages/home';
+// import About from '../../pages/about';
+// import Contact from '../../pages/contact';
 
 class Header extends Component{
     render (){
     return (
-        <Router>
         <div>
-            <nav>
-                <Link path={Home} to="/" >Home</Link>
-                <Link path={About} to="/about" >About</Link>
-                <Link path={Contact} to="/contact" >Contact</Link>
-            </nav>
-            <h1>Header</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>            
+        <h1>Header</h1>
         </div>
-        </Router>            
     )}
 }
 export default Header
