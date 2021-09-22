7876d3817a0f1b65db0b3ae73f50bd493c13e0a5
MWall <mjwall@student.fullsail.edu>
2021-09-22 12:31:11 -0500

    footer

--- app/src/components/footer/index.js
+++ app/src/components/footer/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import styles from './footer.module.css'
+
+import linkedinLogo from '../footer/linkedInlogo.png';
 
 export default function Footer() {
     return(
-        <> 
-        <h1>Footer</h1>
-        </>
+        <div className={styles.footer}> 
+        <p>Copyright © 2021 Megan Petty</p>
+        <a href="https://www.linkedin.com/in/megan-petty-a1258715b/" >
+        <img src={linkedinLogo} alt="linkedin logo by rivda from the Noun Project" />
+        </a>
+        <p>Email me at webdevmeg@gmail.com</p>
+        </div>
     )
 }
