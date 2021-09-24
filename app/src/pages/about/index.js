67d4135ebeb0d956133069e1dcf0f8f2e913d55c
MWall <mjwall@student.fullsail.edu>
2021-09-23 23:27:59 -0400

    redundant divs

--- app/src/pages/about/index.js
+++ app/src/pages/about/index.js
@@ -13,11 +13,9 @@ class About extends Component {
                 <img src={about} className={styles.aboutImage} alt="About Megan" />
             <div className={styles.about}>
             <img className={styles.photo} src={photo} alt="Megan" />
-            <div className={styles.textArea}>
             <p className={styles.description} >Megan started off with a History Degree with a Japanese minor from the University of Mississippi, where she also studied abroad in Tokyo, but that wasn’t enough to sate her quest for knowledge.<br />
                 Now with her second degree in Web Design and Development, she’s dedicated to doing the research and development to find answers to any problem.  Everything is a puzzle to be figured out.<br />
                 A customer service expert, Megan has the technical and personal skills needed to create the foundations to lasting success.  From full scale web applications to live streaming, she can find the best tech for you.</p>
-            </div> 
             </div>
             </div>        
         )
